c38dc089d5f875344647de4b7c127e7097d911a4
Alexandre Moore <alexandre.moore@gmail.com>
2018-09-17 15:30:05 -0400

    Add animations to demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('visibilityChanged', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(500, style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   usersObservable: Observable<any>;
   users: any[] = [];
 
-  constructor(private http: HttpClient, private spinner: NgxSpinnerService) {}
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.spinner.show();
